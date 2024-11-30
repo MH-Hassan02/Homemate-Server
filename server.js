3b63038861a809e0cda0760214e4992293b6a3bd
MH-Hassan02 <haseebhassan1155@gmail.com>
2024-11-30 20:08:37 +0500

    CORS

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
-import uploadRoutes from "./routes/uploadRoutes.js"
-
+import uploadRoutes from "./routes/uploadRoutes.js";
 import favoritesRoutes from "./routes/favoriteRoutes.js";
-// Load environment variables
 dotenv.config();
 
-// Connect to MongoDB
 connectDB();
 
-// Initialize Express
 const app = express();
 
-app.use(cors());
+const allowedOrigins = process.env.FRONTEND_ORIGIN.split(",");
+const corsOptions = {
+  origin: allowedOrigins,
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
-// Routes
 app.get("/", (req, res) => {
   res.json("hello");
 });
@@ -27,9 +27,7 @@ app.use("/auth", authRoutes);
 app.use("/posts", postRoutes);
 
 app.use("/api/upload", uploadRoutes);
-app.use('/api/favorites', favoritesRoutes); 
+app.use("/api/favorites", favoritesRoutes);
 
-
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT} `));
